98d4b78300cfbb76173de0d58e97fec9a77a3be6
Elias-Dewa <elias.dewa19@gmail.com>
2023-12-10 04:08:51 -0800

    Code review

--- Quote-generator/script.js
+++ Quote-generator/script.js
@@ -5,14 +5,12 @@ const twitterButton= document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-// Show Loading Spinner
-function loading() {
+const showLoadingSpinner = () => {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading Spinner
-function complete() {
+const removeLoadingSpinner = () => {
   if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
@@ -20,40 +18,35 @@ function complete() {
 }
 // Get quote from API
 async function getQuote() {
-  loading();
+  showLoadingSpinner();
   const apiUrl = 'http://api.quotable.io/random';
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
+
     // Reduce font size for long quote
-    if (data.content.length > 100) {
-      quoteText.classList.add('long-quote');
-    } else {
-      quoteText.classList.remove('long-quote');
-    }
+    (data.content.length > 100) ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
+
     quoteText.innerText = data.content;
+
     // If the Author is blank, add 'Unknown'
-    if (data.author === '') {
-      authorText.innerText = 'Unknown';
-    } else {
-      authorText.innerText = data.author;
-    }
-    // Stop Loader, Show quote
-    complete();
+    (data.author === '') ? authorText.innerText = 'Unknown' : authorText.innerText = data.author;
+
+    removeLoadingSpinner();
   } catch (error) {
     console.log('Sorry, no quote was found', error);
   }
 }
-// Tweet Quote
-function tweetQuote() {
+
+// Event Listener
+newQuoteButton.addEventListener('click', getQuote);
+
+twitterButton.addEventListener('click', () => {
   const quote = quoteText.innerText
   const author = authorText.innerText
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
   window.open(twitterUrl, '_blank');
-}
+});
 
-// Event Listener
-newQuoteButton.addEventListener('click', getQuote);
-twitterButton.addEventListener('click', tweetQuote);
 // On load
 getQuote();

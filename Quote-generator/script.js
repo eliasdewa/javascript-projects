91e86b0fb5aaf1ecc3e621980d51dd9125cb9179
Elias-Dewa <elias.dewa19@gmail.com>
2023-12-15 03:30:18 -0800

    Adding some other features

--- Quote-generator/script.js
+++ Quote-generator/script.js
@@ -1,24 +1,16 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
+const copyButton= document.getElementById('copy');
+const soundButton= document.getElementById('sound');
 const twitterButton= document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-const showLoadingSpinner = () => {
-  loader.hidden = false;
-  quoteContainer.hidden = true;
-}
-
-const removeLoadingSpinner = () => {
-  if (!loader.hidden) {
-    quoteContainer.hidden = false;
-    loader.hidden = true;
-  }
-}
-// Get quote from API
+// Get random quote from API
 async function getQuote() {
-  showLoadingSpinner();
+  newQuoteButton.classList.add('loading');
+  newQuoteButton.innerText = "Loading Quote...";
   const apiUrl = 'http://api.quotable.io/random';
   try {
     const response = await fetch(apiUrl);
@@ -32,14 +24,24 @@ async function getQuote() {
     // If the Author is blank, add 'Unknown'
     (data.author === '') ? authorText.innerText = 'Unknown' : authorText.innerText = data.author;
 
-    removeLoadingSpinner();
+    newQuoteButton.innerText = "New Quote";
+    newQuoteButton.classList.remove('loading');
   } catch (error) {
     console.log('Sorry, no quote was found', error);
   }
 }
 
 // Event Listener
-newQuoteButton.addEventListener('click', getQuote);
+soundButton.addEventListener('click', () => {
+  // The SpeechSynthesisUtterance is a web speech api that represents a speech requests
+  let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorText.innerText}`);
+  speechSynthesis.speak(utterance);
+});
+
+copyButton.addEventListener('click', () => {
+  // Copy the quote text
+  navigator.clipboard.writeText(quoteText.innerText);
+});
 
 twitterButton.addEventListener('click', () => {
   const quote = quoteText.innerText
@@ -47,6 +49,8 @@ twitterButton.addEventListener('click', () => {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
   window.open(twitterUrl, '_blank');
 });
+
+newQuoteButton.addEventListener('click', getQuote);
 
 // On load
 getQuote();
